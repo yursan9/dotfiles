a4195d20c5080dd9477e953e6282c5cf79d1435b
Yurizal Susanto <yursan9@pm.me>
2019-01-20 09:44:11 +0700

    Update dotfile for Fish 3.0.0
    Delete eopkg completions (because it's became part of
    built-in completion in 3.0.0). Update function for prompt,
    set math --scale to 0; fixes error on printf

--- fish_shell/.config/fish/functions/print_readable_time.fish
+++ fish_shell/.config/fish/functions/print_readable_time.fish
@@ -1,8 +1,8 @@
 function print_readable_time -a duration
 	set -e argv[1]
-	set -l minute (math "$duration / 60000")
-	set -l duration (math "$duration % 60000")
-	set -l second (math "$duration / 1000")
+	set -l minute (math -s0 "$duration / 60000")
+	set -l duration (math -s0 "$duration % 60000")
+	set -l second (math -s0 "$duration / 1000")
 
 	if test $minute -ne 0
 		printf "%dm %ds\n" $minute $second
